519ad29452de55a0e9056bbbbfb4303cbec61ef2
Emerson <emerson29988@gmail.com>
2020-08-30 05:29:27 -0300

    Configuring global exception handler

---
+++ Sample.Core/Exceptions/CustomException.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Sample.Core.Exceptions
+{
+    public class CustomException : Exception
+    {
+        public CustomException()
+        {
+
+        }
+
+        public CustomException(string message) : base(message)
+        {
+
+        }
+
+        public CustomException(string message, Exception innerException) : base(message, innerException)
+        {
+
+        }
+        
+        public CustomException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+
+        }
+    }
+}
